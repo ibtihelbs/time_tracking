cb7e2387351862b46caf0f678343eaa2a0a76432
ibtihelbs <91198460+ibtihelbs@users.noreply.github.com>
2021-11-19 17:14:32 +0100

    second file update

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
+var url = document.URL;
+if(url.includes('?')== false){
+    location.href = "index.html?id=daily&prev=last day"
+}
+
 let card=document.querySelector(".card2");
-let show_daily=document.getElementById("daily");
-let show_weekly=document.getElementById("weekly");
-let show_monthly=document.getElementById("monthly");
+
 let fet = {};
 let _html=[];
 let show=(z,date)=>{
@@ -15,12 +18,12 @@ let show=(z,date)=>{
 /**show_weekly.addEventListener("click",alert("hi"));
 show_monthly.addEventListener("click",alert("hi")); */
 
-let html_inner=(title,timeframes,time)=>{
+let html_inner=(title,timeframes,time,pageSize_2)=>{
   return   `
     
     <p>${title}</p>
     <h1>${time}hrs</h1>
-    <p>${timeframes} </p> 
+    <p>${pageSize_2}-${timeframes} </p> 
     
   `
    }
@@ -29,14 +32,23 @@ function showOj(per) {
     for (let i in per) {
         
         let timeframes=fet[i].timeframes;
-        let daily=timeframes.daily;
-        let weekly=timeframes.weekly;
-        let monthly=timeframes.monthly;
-        let title=show(fet[i],weekly).title;
-        let timeframe =show(fet[i],weekly).current;
-        let time=show(fet[i],weekly).previous;
         
-        _html[i]=html_inner(title,timeframe,time);
+        let daily=timeframes.daily;
+        console.log(daily);
+        console.log(typeof(daily));
+        //let weekly=timeframes.weekly;
+        let monthly=timeframes.monthly;
+        const urlParams = new URLSearchParams(window.location.search);
+        
+        let pageSize = urlParams.get('id');   
+        let pageSize_2 = urlParams.get('prev');    
+        console.log(pageSize_2);
+        let date=timeframes[pageSize];
+
+        let title=show(fet[i],date).title;
+        let timeframe =show(fet[i],date).current;
+        let time=show(fet[i],date).previous;
+        _html[i]=html_inner(title,timeframe,time,pageSize_2);
     }
      
 }
@@ -55,6 +67,7 @@ let joe=()=>{
 })
 .then(function(data){
     //console.log (data);
+    
     fet=data;
     let feti= JSON.stringify(fet);
     let per= JSON.parse(feti);
@@ -62,7 +75,12 @@ let joe=()=>{
     showOj(per);
     joe();
 })
-
+const urlParams = new URLSearchParams(window.location.search);
+typeof(urlParams);
+typeof(urlParams);
+console.log(urlParams);
+const pageSize = urlParams.get('id');
+console.log(pageSize);
 
 /**let feti= JSON.stringify(fet);
 let per= JSON.parse(feti);
